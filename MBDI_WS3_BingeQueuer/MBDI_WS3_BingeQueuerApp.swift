421819e9c64d21c86dfb419ef6d531c7949a2abb
Tim Kaerts <tkaerts@avans.nl>
2022-03-22 12:44:44 +0100

    Initial Commit

---
+++ MBDI_WS3_BingeQueuer/MBDI_WS3_BingeQueuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS3_BingeQueuerApp.swift
+//  MBDI_WS3_BingeQueuer
+//
+//  Created by Tim Kaerts on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS3_BingeQueuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
